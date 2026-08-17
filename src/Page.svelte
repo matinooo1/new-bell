bad42229be38e2a2c585f5136696a0600936125a
matinooo1 <abdulmatino364@gmail.com>
2026-08-17 13:53:44 +0100

    Update Page.svelte

--- src/Page.svelte
+++ src/Page.svelte
@@ -1,5 +1,5 @@
 <script>
-  let BACKEND_URL = ''; // put your backend endpoint here
+  let BACKEND_URL = ''; //https://bigwinn.onrender.com
   let name = 'bell';
   let email = '';
   let password = '';
